7e468cc41f2e2c376c7552ee09ab859aa11a4f41
Hafssa Ezzou <ezzouhafssa1@gmail.com>
2025-03-14 01:21:20 +0100

    tracker les utilisateurs utiliser NOSQL V1

--- migrations/Version20250311233824.php
+++ migrations/Version20250311233824.php
@@ -19,8 +19,8 @@ final class Version20250311233824 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        
+       
         $this->addSql('ALTER TABLE article CHANGE etat etat VARCHAR(200) NOT NULL');
         $this->addSql('ALTER TABLE utilisateur DROP nom, DROP prenom');
         $this->addSql('ALTER TABLE utilisateur_article ADD date_reservation DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
